eff79c20748e9e8d5900b59c0bed427bebabda96
oeun <oeun2@greenlabs.co.kr>
2023-09-05 13:39:10 +0900

    chore: add cypress

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? {exclude: ['error', 'wran']} : false,
+  }
 }
 
 module.exports = nextConfig
